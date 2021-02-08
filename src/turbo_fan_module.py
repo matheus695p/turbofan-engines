a6ce45b197fbff86463a87a4171db7f9da6881a9
matheus695p <mateu_695p@hotmail.com>
2021-02-07 23:27:29 -0300

    add: modulo de funciones

---
+++ src/turbo_fan_module.py
@@ -0,0 +1,28 @@
+import numpy as np
+from sklearn.metrics import mean_squared_error, r2_score
+
+
+def add_remaining_useful_life(df):
+    # Get the total number of cycles for each unit
+    grouped_by_unit = df.groupby(by="unit_nr")
+    max_cycle = grouped_by_unit["time_cycles"].max()
+
+    # Merge the max cycle back into the original frame
+    result_frame = df.merge(max_cycle.to_frame(
+        name='max_cycle'), left_on='unit_nr', right_index=True)
+
+    # Calculate remaining useful life for each row
+    remaining_useful_life = result_frame["max_cycle"] - \
+        result_frame["time_cycles"]
+    result_frame["RUL"] = remaining_useful_life
+
+    # drop max_cycle as it's no longer needed
+    result_frame = result_frame.drop("max_cycle", axis=1)
+    return result_frame
+
+
+def evaluate(y_true, y_hat, label='test'):
+    mse = mean_squared_error(y_true, y_hat)
+    rmse = np.sqrt(mse)
+    variance = r2_score(y_true, y_hat)
+    print('{} set RMSE:{}, R2:{}'.format(label, rmse, variance))
